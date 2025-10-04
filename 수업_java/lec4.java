7c31377c485290c2fdac4b58c837651e9ca14d60
232585981 <sdfjasoidfjio@users.noreply.github.com>
2025-10-04 21:13:55 +0900

    추가

--- 수업_java/lec4.java
+++ 수업_java/lec4.java
@@ -330,7 +330,7 @@ class CurrencyConverter {
 class StaticMember {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("환율(1달러)>> ");
+        System.out.print("환율(1달러)>>");
         double rate = sc.nextDouble();
         CurrencyConverter.setRate(rate);
         System.out.println("백만원은 $" + CurrencyConverter.toDollar(1000000) + "입니다.");
