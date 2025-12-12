7e9602fbe6265b081c85212ae12b0c654d9636be
sdfjasoidfji <232585981+sdfjasoidfjio@users.noreply.github.com>
2025-12-13 03:02:09 +0900

    추가

---
+++ 기말대비_java/a71.java
@@ -0,0 +1,25 @@
+package 기말대비_java;
+import java.util.*;
+
+public class a71 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Vector<Integer> v = new Vector<Integer>();
+
+        System.out.print("정수(-1이 입력될 때까지)>> ");
+        while(true){
+            int n = sc.nextInt();
+            if(n==-1){
+                break;
+            }
+            v.add(n);
+        }
+        int max = v.get(0);
+        for(int i = 1; i < v.size(); i++){
+            if (max < v.get(i)){
+                max = v.get(i);
+            }
+        }
+        System.out.println("가장 큰 수는 "+ max);
+    }
+}
